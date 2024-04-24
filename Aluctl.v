9ddedb2cb85c34194abc5eb506aed7936c0386d6
Jose Herrera <josexd96@gmail.com>
2024-04-24 14:42:09 -0600

    benching

--- Aluctl.v
+++ Aluctl.v
@@ -4,23 +4,27 @@ module ALUcontrol (ALUOp, FuncCode, ALUCtl);
     input [5:0] FuncCode;
     output [3:0] ALUCtl;
     
-    
-    if (ALUOp==2)begin
-        always case (FuncCode)  
+     /*
+        always case (FuncCode)
         32:ALUCtl<=2;
         34:ALUCtl<=6;
         36:ALUCtl<=0;
         37:ALUCtl<=1;
         default: ALUOp<=15;
         endcase
-    end
 
-    if (ALUOp==0)begin  
-        aALUCtl=2;
-    end
+        */
 
-    if (ALUOp==1)begin
-        ALUCtl=6;        
+    always begin @(posedge clock)
+    if (ALUOp==2)
+    
+        if (ALUOp==0)
+            ALUCtl<=2;
+
+        else if (ALUOp==1)
+            ALUCtl<=6;        
+ 
+
     end
 
 endmodule
