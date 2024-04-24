cdb328e0082cefa8d27605b8b6e247e5fd91ff4b
Jose Herrera <josexd96@gmail.com>
2024-04-23 18:18:00 -0600

    benching

--- registerfile.v
+++ registerfile.v
@@ -6,12 +6,12 @@ module registerfile(Read1,Read2,WriteReg,WriteData,RegWrite,Data1,Data2,clock);
     output [31:0] Data1, Data2;
     reg [31:0] RF [31:0];
 
-
-    always begin  
-        @(posedge clock) 
-        if (RegWrite) 
-            RF[WriteReg]<=WriteData;
-    end
     assign Data1=RF[Read1];
     assign Data2=RF[Read2];
+
+
+    always begin @(posedge clock) 
+        if (RegWrite==1) 
+            RF[WriteReg]<=WriteData;
+    end
 endmodule
--- registerfile_tb.v
+++ registerfile_tb.v
@@ -1,8 +1,8 @@
 `include "registerfile.v"
-`timescale 1us/1ns
+//`timescale 1ns / 1ps
 module registerfile_tb;
 
-    parameter CLK_PERIOD = 10; 
+    parameter CLK_PERIOD = 10000; 
 
     reg [5:0] Read1, Read2, WriteReg;
     reg [31:0] WriteData;
@@ -29,15 +29,22 @@ module registerfile_tb;
     always #((CLK_PERIOD)/2) clock = ~clock;
 
      initial begin
-       
+
+        clock=0;
+        RegWrite = 1; 
+
         WriteReg = 3; 
         WriteData = 32'hABCDEFFF; 
-        RegWrite = 1; 
+        #CLK_PERIOD;
+
+        WriteReg = 5; 
+        WriteData = 32'hFBCDE111; 
+        #CLK_PERIOD;
+
+        RegWrite = 0;
         Read1 = 3; 
         Read2 = 5; 
         
-        #CLK_PERIOD;
-        RegWrite = 0;
         #CLK_PERIOD;
         Read1 = 7;
         Read2 = 10;
@@ -45,4 +52,9 @@ module registerfile_tb;
         $finish;
     end
 
+     always @* begin
+
+        $display("data1: %h,  data2: %h", Data1,Data2);
+    end
+    
 endmodule
