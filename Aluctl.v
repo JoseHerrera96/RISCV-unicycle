6fbe65c169acfd1f8b81c3cbddf54284fdcc02a1
Jose Herrera <josexd96@gmail.com>
2024-04-24 21:53:15 -0600

    AluCTRL

--- Aluctl.v
+++ Aluctl.v
@@ -2,29 +2,26 @@ module ALUcontrol (ALUOp, FuncCode, ALUCtl);
 
     input [1:0] ALUOp;
     input [5:0] FuncCode;
-    output [3:0] ALUCtl;
+    output reg [3:0] ALUCtl;
     
-     /*
-        always case (FuncCode)
-        32:ALUCtl<=2;
-        34:ALUCtl<=6;
-        36:ALUCtl<=0;
-        37:ALUCtl<=1;
-        default: ALUOp<=15;
+     
+        always @*
+        if (ALUOp==2) begin
+            case (FuncCode)
+            32:ALUCtl<=2;
+            34:ALUCtl<=6;
+            36:ALUCtl<=0;
+            37:ALUCtl<=1;
+            
+            default: ALUCtl<=15;
         endcase
+        end
 
-        */
-
-    always begin @(posedge clock)
-    if (ALUOp==2)
-    
-        if (ALUOp==0)
-            ALUCtl<=2;
-
-        else if (ALUOp==1)
-            ALUCtl<=6;        
- 
-
-    end
+        else if (ALUOp==1)begin
+            ALUCtl=3;
+        end
+        else if (ALUOp==0)begin
+            ALUCtl=2;
+        end
 
 endmodule
