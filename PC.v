249e1ffbf7b6aac265276066a3039e1d70716265
Jose Herrera <josexd96@gmail.com>
2024-04-27 23:30:06 -0600

    updates PC module

--- PC.v
+++ PC.v
@@ -1,19 +1,18 @@
-module PC (clk,reset,pc_out);
-input wire clk,
-input wire reset,
-output reg [31:0] pc_out
+module PC (clk,reset, pc_reg);
+input clk, reset;
 // Registro para el contador de programa
-reg [31:0] pc_reg;
+output reg [31:0] pc_reg;
 
 // Lógica del contador de programa
-always @(posedge clk, posedge reset)
-begin
-    if (reset)
-        pc_reg <= 32'h0;
-    else
-        pc_reg <= pc_reg + 4;
-end
 
-assign pc_out = pc_reg;
+always begin @(posedge clk, posedge reset)
+
+    if (reset==1)
+        pc_reg <= 32'h0;
+    
+    else
+        pc_reg <= pc_reg + 32'h4;
+
+end
 
 endmodule
